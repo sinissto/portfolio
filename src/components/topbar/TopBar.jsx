d40cf0b17d79f23ba655115c3e5ecd15939ed4cd
Sinisa <sinisha.stojanovic@gmail.com>
2022-12-10 13:47:26 +0100

    Left part of TopBar completed

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,14 +1,25 @@
 import './TopBar.scss';
 
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
+import PhoneIcon from '@mui/icons-material/Phone';
+
 const TopBar = () => {
   return (
     <div className="topbar">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            Sinisa's Portfolio
+            genious.
           </a>
-          <div className="itemContainer"></div>
+          <div className="itemContainer">
+            <PhoneIcon className="icon" />
+            <span>+381 63 389 762</span>
+          </div>
+          <div className="itemContainer">
+            <MailIcon className="icon" />
+            <span>sinisha.stojanovic@gmail.com</span>
+          </div>
         </div>
         <div className="right">ovo je desno</div>
       </div>
